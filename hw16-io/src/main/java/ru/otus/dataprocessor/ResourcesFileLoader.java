1c768d117d2e53e4f06a0e0acd14dda824c5b7c1
Evgeny0088 <evgeny.kuznetsov0088@gmail.com>
2021-12-28 17:37:13 +0300

    gson implementation added for loading resource file

--- hw16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw16-io/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -7,9 +7,12 @@ import ru.otus.model.Measurement;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import com.google.gson.Gson;
 
 public class ResourcesFileLoader implements Loader {
 
@@ -27,6 +30,17 @@ public class ResourcesFileLoader implements Loader {
 
     @Override
     public List<Measurement> load() throws IOException {
+        URL inputDataJson = ResourcesFileLoader.class.getClassLoader().getResource(fileName);
+        assert inputDataJson != null;
+        try(Reader jsonReader = new BufferedReader(new FileReader(inputDataJson.getFile()))){
+            var gson = new Gson();
+            return Arrays.asList(gson.fromJson(jsonReader,Measurement[].class));
+        }catch (FileProcessException exception){
+            throw new FileProcessException("file cannot be processed!");
+        }
+    }
+
+    private List<Measurement> loadJacksonImplementationBackUp() throws IOException {
         mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         List<Measurement> measurements = new ArrayList<>();
         URL inputDataJson = ResourcesFileLoader.class.getClassLoader().getResource(fileName);
@@ -45,4 +59,5 @@ public class ResourcesFileLoader implements Loader {
         });
         return measurements;
     }
+
 }
