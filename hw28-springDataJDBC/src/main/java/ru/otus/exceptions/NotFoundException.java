3ae56003a0ae6d763bbbfbb38204aa2a40a770fb
Evgeny0088 <evgeny.kuznetsov0088@gmail.com>
2022-02-15 18:39:55 +0300

    NotFoundException.java small change

--- hw28-springDataJDBC/src/main/java/ru/otus/exceptions/NotFoundException.java
+++ hw28-springDataJDBC/src/main/java/ru/otus/exceptions/NotFoundException.java
@@ -2,6 +2,6 @@ package ru.otus.exceptions;
 
 public class NotFoundException extends RuntimeException {
     public<T> NotFoundException(Long id, T item ) {
-        super(String.format("%s with id:%d is not found", item, id));
+        super(String.format("%s с id:%d не найден", item, id));
     }
 }
