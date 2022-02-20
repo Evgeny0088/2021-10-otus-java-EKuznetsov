fdaf660a84337a00efb0ffe125e7f3311d051e78
Evgeny0088 <evgeny.kuznetsov0088@gmail.com>
2022-02-20 17:35:32 +0300

    build folders are removed

--- hw31-executors/src/main/java/ru/otus/NumberStreams.java
+++ hw31-executors/src/main/java/ru/otus/NumberStreams.java
@@ -1,7 +1,6 @@
 package ru.otus;
 
 public class NumberStreams {
-
     private static final int STREAM_LIMIT = 10;
     private volatile int forwardIndex = 1;
     private volatile int backwardIndex = STREAM_LIMIT-1;
